f053f4976844a9919be6d4b08fb0d4a865f9f26f
Alon Emanuel <alon@alonemanuel.com>
2019-04-19 18:01:32 +0300

    Now with alert box

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,62 @@
-package sample;
-
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Stage window;
+    Scene scene1, scene2;
+    Button button;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+
+        window = primaryStage;
+        Label label1 = new Label("Welcome to the first scene!");
+
+        // button 1
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
+
+        // button 2
+        Button button2 = new Button("This scene sucks. Go back to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        // layout 1 - children are laid out in vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
+
+        // layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        window.setScene(scene1);
+        window.setTitle("Title here");
+        window.show();
+
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.setTitle("Title of the Window");
+//
+//        button = new Button();
+//        button.setText("Click me");
+//        button.setOnAction(e -> System.out.println("Yo why you pressin"));       // look in this class for handle method
+//
+//        StackPane layout = new StackPane();
+//        layout.getChildren().add(button);
+//
+//        Scene scene = new Scene(layout, 300, 275);
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
     }
 
 
